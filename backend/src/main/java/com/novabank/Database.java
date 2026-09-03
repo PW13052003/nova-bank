e54fe17718f12051aa1a9a88511f78a217979f92
Puranjay Wadhera <pw425@drexel.edu>
2026-09-03 10:44:56 -0400

    Fix Database class to read env vars directly, resolving production login failure

--- backend/src/main/java/com/novabank/Database.java
+++ backend/src/main/java/com/novabank/Database.java
@@ -1,31 +1,15 @@
 package com.novabank;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class Database {
 
-    private static Properties loadConfig() throws IOException {
-        Properties props = new Properties();
-        try (InputStream input = Database.class.getClassLoader()
-                .getResourceAsStream("application.properties")) {
-            if (input == null) {
-                throw new IOException("application.properties not found on classpath");
-            }
-            props.load(input);
-        }
-        return props;
-    }
-
-    public static Connection getConnection() throws SQLException, IOException {
-        Properties props = loadConfig();
-        String url = props.getProperty("spring.datasource.url");
-        String username = props.getProperty("spring.datasource.username");
-        String password = props.getProperty("spring.datasource.password");
+    public static Connection getConnection() throws SQLException {
+        String url = System.getenv("DATABASE_URL");
+        String username = System.getenv("DATABASE_USERNAME");
+        String password = System.getenv("DATABASE_PASSWORD");
         return DriverManager.getConnection(url, username, password);
     }
 }
